3c69145516e1c93428cbb3ea2cef281603654291
vali2003 <murthujavali465@gmail.com>
2024-12-27 21:23:57 +0530

    react methods

---
+++ HTTP/src/App.jsx
@@ -0,0 +1,14 @@
+import { Posts } from "./components/Posts";
+
+function App() {
+
+  return (
+    <>
+      <section className="main-section">
+      <Posts />
+    </section>
+    </>
+  )
+}
+
+export default App;
